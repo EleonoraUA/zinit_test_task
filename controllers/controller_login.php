75a21bc8560e3bfc9317716a093bd1c4def48d7e
eleonoria96@gmail.com <eleonoria96@gmail.com>
2016-08-30 22:57:57 +0300

    Search for user in the database

--- controllers/controller_login.php
+++ controllers/controller_login.php
@@ -3,31 +3,63 @@
 class Controller_Login extends Controller
 {
 
-	function __construct()
-	{
-		$this->view = new View();
-	}
-	
-	function action_index()
-	{
-		$email = $_POST['email'];
-		$password = $_POST['password'];
+    const HOST = "127.0.0.1";
+    const DB_NAME = "users";
+    const DB_USER = "mg"; // Change user and password to yours to get it works
+    const DB_PASSWORD = "meangirls";
 
-		if ($this->isValidEmail($email) && $this->isValidPassword($password)) {
-			echo json_encode(array('status' => true));
-			// TODO safe to database
-		} else {
-			echo json_encode(array('status' => false));
-		}
-	}
+    private $pdo;
+//
+//	function __construct()
+//	{
+//		$this->view = new View();
+//	}
 
-	private function isValidEmail($email)
-	{
-		return preg_match('/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email);
-	}
+    private function getPDO()
+    {
+        if (!isset($this->pdo)) {
+            $dsn = "mysql:host=" . self::HOST . ";dbname=" . self::DB_NAME . ";";
+            $opt = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            );
+            $this->pdo = new PDO($dsn, self::DB_USER, self::DB_PASSWORD, $opt);
+        }
+        return $this->pdo;
+    }
 
-	private function  isValidPassword($password)
-	{
-		return preg_match('/^[a-zA-Z\d]+$/', $password);
-	}
+    function action_index()
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if ($this->isValidEmail($email) && $this->isValidPassword($password)) {
+            $stmt = $this->getPDO()->prepare('SELECT id, last_visit, password FROM user WHERE email = ?');
+            $stmt->execute(array($email));
+            $row = $stmt->fetchColumn();
+            if (isset($row['password']) && $this->passwordsAreEqual($password, $row['password'])) {
+                echo json_encode(array('status' => true, 'id' => $row['id'], 'last_visit' => $row['last_visit']));
+                // todo update last_visit
+            }
+        } else {
+            echo json_encode(array('status' => false));
+        }
+    }
+
+    private function passwordsAreEqual($userPassword, $dbPassword)
+    {
+        $salt = 'zinit'; // this string is used for safety, salt is adding while writing password to db
+
+        return md5($salt . $userPassword) === $dbPassword;
+    }
+
+    private function isValidEmail($email)
+    {
+        return preg_match('/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email);
+    }
+
+    private function  isValidPassword($password)
+    {
+        return preg_match('/^[a-zA-Z\d]+$/', $password);
+    }
 }
--- views/template_view.php
+++ views/template_view.php
@@ -2,17 +2,56 @@
 
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="../styles/bootstrap.min.css">
-    <script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="/views/styles/bootstrap.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"
+            integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+    <script src="/views/jquery/jquery.validationEngine.js"></script>
+    <script src="/views/jquery/jquery.validationEngine-en.js"></script>
+    <link rel="stylesheet" href="/views/styles/validationEngine.jquery.css"/>
+    <script>
+        $(document).ready(function () {
+            $("#login_form").validationEngine('attach');
+            $("#login_submit").click(function (event) {
+
+                event.preventDefault();
+
+                var email = $('#user_email').val();
+                var password = $('#user_password').val();
+
+                $.ajax({
+                    url: '/Login/index/',
+                    data: "email=" + email + "&password=" + password,
+                    type: 'POST',
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data['status']) {
+                            $('#info').append('<div class = "alert alert-success"> Success! Id:' + data['id'] + ' Last visit:' + data['last_visit'] + '</div>');
+                        } else {
+                            alert("Oops... Not valid email or password");
+                        }
+                    }
+                });
+            });
+        });
+    </script>
 </head>
 <body>
 <div class="container">
     <div class="row">
         <div class="col-lg-4 col-lg-offset-4">
-            <form name="login_form" action="/Login/index/">
+            <div id="info">
+                <div class=" alert alert-info">
+                    <h4>
+                        Please log in with <b>test@example.com</b> and <b>12345</b>
+                    </h4>
+                </div>
+            </div>
+            <form name="login" action="/Login/index/" id="login_form">
                 <div class="form-group">
                     <label for="user_email">Email address</label>
-                    <input type="email" class="form-control" id="user_email" placeholder="Email">
+                    <input type="email" class="form-control validate[required, custom[email]]" id="user_email"
+                           placeholder="Email">
                 </div>
                 <div class="form-group">
                     <label for="user_password">Password</label>
@@ -25,27 +64,4 @@
 </div>
 </body>
 
-<script>
-    $("#login_submit").click(function (event) {
-
-        event.preventDefault();
-
-        var email = $('#user_email').val();
-        var password = $('#user_password').val();
-
-        $.ajax({
-            url: '/Login/index/',
-            data: "email="+email+"&password="+password,
-            type: 'POST',
-            dataType: 'json',
-            success: function(data){
-                if (data['status']) {
-                    alert("You have successfully logged in");
-                } else {
-                    alert("Oops... Not valid email or password");
-                }
-            }
-        });
-    });
-</script>
 </html>
